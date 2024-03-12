0955e969942be10d19465eee549f31699a44d6c5
kariithisamuel <146923686+kariithisamuel@users.noreply.github.com>
2024-03-12 17:26:41 +0300

    Update RestaurantApi.tsx

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -1,5 +1,5 @@
 import { SearchState } from "@/Pages/SearchPage";
-import { Restaurant, RestaurantSearchResponse } from "@/types";
+import { Restaurant } from "@/types";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -28,36 +28,4 @@ export const useGetRestaurant = (restaurantId?: string) => {
   return { restaurant, isLoading };
 };
 
-export const useSearchRestaurants = (
-  searchState: SearchState,
-  city?: string
-) => {
-  const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
-    const params = new URLSearchParams();
-    params.set("searchQuery", searchState.searchQuery);
-    params.set("page", searchState.page.toString());
-    params.set("selectedCuisines", searchState.selectedCuisines.join(","));
-    params.set("sortOption", searchState.sortOption);
-
-    const response = await fetch(
-      `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to get restaurant");
-    }
-
-    return response.json();
-  };
-
-  const { data: results, isLoading } = useQuery(
-    ["searchRestaurants", searchState],
-    createSearchRequest,
-    { enabled: !!city }
-  );
-
-  return {
-    results,
-    isLoading,
-  };
 };
