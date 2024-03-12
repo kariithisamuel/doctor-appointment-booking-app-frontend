7269f859afc697516106ff310b84d39cd0349c01
kariithi-samuel <samuelmainak70@gmail.com>
2024-03-12 15:46:03 +0300

    deploying users profile

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,16 +4,38 @@ import HomePage from "./Pages/HomePage";
 import AuthCallbackPage from "./Pages/AuthCallbackPage";
 import UserProfilePage from "./Pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./Pages/ManageRestaurantPage";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
+            <Route
+                path="/"
+                element={
+                    <Layout showHero>
+                        <HomePage />
+                    </Layout>
+                }
+            />
 
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
-
-            <Route element={<ProtectedRoute />}>
-                <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
+            <Route
+                element={<ProtectedRoute />}>
+                <Route
+                    path="/user-profile"
+                    element={<Layout>
+                        <UserProfilePage />
+                    </Layout>
+                    }
+                />
+                 <Route
+                    path="/manage-restaurant"
+                    element={<Layout>
+                        <ManageRestaurantPage/>
+                    </Layout>
+                    }
+                />
+                
             </Route>
             
             <Route path="*" element={<Navigate to="/" />} />
